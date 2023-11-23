479250af3e0f25b66c62e7ba94319ed47472621f
pjseok <pjs7850@naver.com>
2023-11-23 14:11:06 +0900

    Array 연습문제 추가

---
+++ src/ch05_2/ArrayExer.java
@@ -0,0 +1,22 @@
+package ch05_2;
+
+public class ArrayExer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int max = 0;
+		int[] array = {1,5,3,8,2};
+		
+		for(int i = 0;i < array.length;i++){
+			if (max < array[i]) {
+					max = array[i];
+				}
+			}
+		System.out.println("max : "+max);
+		
+		
+		}
+		
+	}
+
+
