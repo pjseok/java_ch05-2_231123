756adbf32d5defc3c8252b09588ebd5297074e32
pjseok <pjs7850@naver.com>
2023-11-23 14:22:08 +0900

    ArrayExer 추가

--- src/ch05_2/ArrayExer.java
+++ src/ch05_2/ArrayExer.java
@@ -5,7 +5,7 @@ public class ArrayExer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int max = 0;
-		int[] array = {1,5,3,8,2};
+		int[] array = {1,8,3,4,2};
 		
 		for(int i = 0;i < array.length;i++){
 			if (max < array[i]) {
